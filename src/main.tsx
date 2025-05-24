29aa09bc8ac5cd4315ef40d606c5dc751856ce9f
Evelyn Kumeh <evelynkumeh@Evelyns-Air.lan>
2025-05-23 19:47:10 -0500

    update react plugin for extension3

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import App from './App.tsx'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
